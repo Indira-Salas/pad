2ba84fd77d821b611769eac11eba6dc00e861b47
Indira-Salas <indira.salas@est.iudigital.edu.co>
2025-03-15 00:31:30 +0000

    Primer Commit

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="actividad_2",
+    version="0.0.1",
+    authors="Indira Salas Garcia",
+    author_email="indira.salas@est.iudigital.edu.co",
+    description="",
+    py_modules=["actividad_2"],
+    install_requires=[
+        "pandas",
+        "openpyxl",
+        "requests"
+    ]
+) 
